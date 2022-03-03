324e480b466b2e3dffa2c51602ca52c989db6110
Wen Lu <zpxo1928@gmail.com>
2022-03-03 14:41:00 +0800

    profile

---
+++ assets/js/sup.js
@@ -0,0 +1,55 @@
+// let data = [];
+
+// const sendBtn = document.querySelector('.button');
+// // console.log(sendBtn);
+// const name = document.querySelector('#name');
+// const email = document.querySelector('#email');
+// const subject = document.querySelector('#subject');
+// const message = document.querySelector('#message');
+
+
+// sendBtn.addEventListener('click', function (e) {
+//     console.log(e);
+
+//     data.push({
+//         "name": name.value,
+//         "email": email.value,
+//         "subject": subject.value,
+//         "message": message.value
+//     })
+
+//     console.log(data);
+
+//     // Email.send({
+//     //     Host : "smtp.yourisp.com",
+//     //     Username : "username",
+//     //     Password : "password",
+//     //     To : 'them@website.com',
+//     //     From : "you@isp.com",
+//     //     Subject : "This is the subject",
+//     //     Body : "And this is the body"
+//     // }).then(
+//     //   message => alert(message)
+//     // );
+
+//     // Email.send({
+//     //     SecureToken : "3d8418e2-1403-4802-b18d-6cb3f9bbfeb1",
+//     //     To : 'tender0026@gmail.com',
+//     //     From : `${email.value}`,
+//     //     Subject : `${subject.value}`,
+//     //     Body : `${message.value}`
+//     // }).then(
+//     //   message => alert(message)
+//     // );
+
+//     name.value = "";
+//     email.value = "";
+//     subject.value = "";
+//     message.value = "";
+
+//     swal("success!", "You send the message!", "success");
+
+
+// })
+
+
